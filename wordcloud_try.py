38079347964c4dd59dcdca716966d33e6b5bc6a8
wuweibuyi <renlg10@163.com>
2017-05-06 15:40:55 +0800

    add pic and show with color

--- wordcloud_try.py
+++ wordcloud_try.py
@@ -2,26 +2,41 @@
 
 from os import path
 import os
+import numpy as np
 import jieba
-from wordcloud import WordCloud
+from PIL import Image
+from wordcloud import WordCloud , ImageColorGenerator
 import matplotlib.pyplot as plt
 
 #获取文本内容
 d = path.dirname(__file__)
 text_content = open(path.join( d , 'text.txt')).read().decode('gbk')
+alice_mask=np.array(Image.open(path.join(d,"bear.jpg")))
 
 #jieba分词处理
 word_list=jieba.cut(text_content,cut_all=False)
 word_split=" ".join(word_list)
 
-#生成词云1，并展示
-wordcloud_image = WordCloud().generate(word_split)
+
+
+#生成词云，并展示
+wordcloud_image = WordCloud(background_color='white',mask=alice_mask ,max_words=5000,\
+                            width=80,height=40,max_font_size=30,min_font_size=5\
+                            ).generate(word_split)
+image_colors = ImageColorGenerator(alice_mask)
+
+'''
 plt.imshow(wordcloud_image)
+plt.axis("off")
+'''
+plt.figure('1')
+plt.imshow(wordcloud_image.recolor(color_func=image_colors))
 plt.axis("off")
 
-#生成词云2，并展示
-wordcloud_image = WordCloud(width=800,height=400,max_font_size=84,min_font_size=16).generate(word_split)
-plt.figure()
-plt.imshow(wordcloud_image)
+plt.figure('2')
+plt.imshow(alice_mask)
 plt.axis("off")
+
 plt.show()
+
+wordcloud_image.to_file(path.join(d,"alice.png"))
